973821c5563948e647a5bc0973d257e15d84af95
Florencia Montenegro <mflortrabajos@gmail.com>
2022-08-29 19:50:59 -0300

    Desafio Array

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let numero1 = parseInt(prompt("INGRESE EL PRIMER NUMERO DE LA OPERACION"));
+/* let numero1 = parseInt(prompt("INGRESE EL PRIMER NUMERO DE LA OPERACION"));
 let operacion = prompt("INGRESE EL OPERADOR (+) (-) (*) (/)");
 let numero2 = parseInt(prompt("INGRESE EL SEGUNDO NUMERO DE LA OPERACION"));
 
@@ -25,4 +25,33 @@ let numero2 = parseInt(prompt("INGRESE EL SEGUNDO NUMERO DE LA OPERACION"));
         break;
     }
 
-alert(resultado);
+alert(resultado); */
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre  = nombre.toUpperCase();
+        this.precio  = parseFloat(precio);
+        this.vendido = false;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+}
+//Almaceno objetos
+const productos = [];
+productos.push(new Producto("arroz", "125"));
+productos.push(new Producto("fideo", "110"));
+productos.push(new Producto("pan", "100"));
+productos.push(new Producto("leche", "180"));
+productos.push(new Producto("azucar", "120"));
+productos.push(new Producto("yerba", "300"));
+
+for (const producto of productos) {
+    console.log('ANTES - '+producto.nombre +': $'+  producto.precio);
+}
+//Modifico el valor
+for (const producto of productos){
+
+    producto.sumaIva();
+    console.log('DESPUES - '+producto.nombre +': $'+  producto.precio);
+}
