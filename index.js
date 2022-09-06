1436970d79d8ff79a007c004a7a7d95c74be87a5
Florencia Montenegro <mflortrabajos@gmail.com>
2022-09-06 19:59:00 -0300

    calculadora de presupuesto

--- index.js
+++ index.js
@@ -1,36 +1,110 @@
-let operacion;
-    document.querySelector('#suma').addEventListener('click', () =>{
-        operacion = '+';
-    });
-    document.querySelector('#resta').addEventListener('click', () =>{
-        operacion = '-';
-    });
-    document.querySelector('#multiplicacion').addEventListener('click', () =>{
-        operacion = '*';
-    });
-    document.querySelector('#division').addEventListener('click', () =>{
-        operacion = '/';
-    });
-    document.querySelector('#borrar').addEventListener('click', () =>{
-        n.textContent = "";
-        numero1 = "";
-        numero2 = "";
-    });
-   
+const formulario = document.querySelector('#añadirGasto');
+const listadoGastos = document.querySelector('#gastos ul');
 
-    document.querySelector('#calcular').addEventListener('click', () =>{
-        const numero1 = parseInt(document.querySelector('#numero1').value);
-        const numero2 = parseInt(document.querySelector('#numero2').value);
-        let n;
-        if(operacion == '+'){
-            n = numero1 + numero2;
-        }else if( operacion == '-'){
-            n = numero1 - numero2;
-        }else if(operacion == '*'){
-            n = numero1 * numero2;
-        }else if(operacion == '/'){
-            n = numero1 / numero2;
+//EVENTOS
+
+acciones();
+
+function acciones(){
+    document.addEventListener('DOMContentLoaded', presupuestoEstimado);
+    formulario.addEventListener('submit', añadirGasto);
+}
+
+//CALCULOS
+//CALCULO PRINCIPAL
+class Presupuesto {
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+
+// AÑADIR NUEVO GASTO
+newGasto(gasto){
+    this.gastos = [...this.gastos, gasto];
+    this.valorRestante();
+}
+// CALCULAR RESTANTE, LUEGO DE UN NUEVO GASTO
+valorRestante(){
+    const dineroGastado = this.gastos.reduce((total,gasto) => total + gasto.cantidad, 0);
+    this.restante = this.presupuesto - dineroGastado;
+}}
+ let presupuesto;
+//CREAR UNO PARA ELIMINAR UN GASTO
+
+//
+class Idx {
+    agregarPresupuesto(cantidad){
+        const {presupuesto, restante} = cantidad;
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    mostrarGasto(mensaje, tipo){
+        const divMsj = document.createElement('div');
+        divMsj.classList.add('text-center', 'alert');
+        if(tipo === 'error'){
+            divMsj.classList.add('alert-danger');
+        }else{
+            divMsj.classList.add('alert-success');
+
         }
-        document.querySelector('#n').innerHTML = n;
-        
-    });
+        divMsj.textContent = mensaje;
+        document.querySelector('.primario').insertBefore(divMsj, formulario);
+//BORRAR EL MENSAJE
+        setTimeout(() => {
+            divMsj.remove();
+        }, 2000);
+    }
+
+
+    agregarGasto(gastos){
+        this.listHtml();
+        gastos.forEach( gasto => {
+            const {cantidad, nombre, id} = gasto;
+
+            //crear lista
+            const newGasto = document.createElement('li');
+            newGasto.className = ' d-flex align-items-center'
+            newGasto.dataset.id = id;
+            newGasto.innerHTML = `${nombre} - $${cantidad}` ;
+            listadoGastos.appendChild(newGasto);
+            
+        });
+    }
+    listHtml(){
+        while(listadoGastos.firstChild){
+            listadoGastos.removeChild(listadoGastos.firstChild);
+        }
+    }
+
+    newRestante(restante){
+        document.querySelector('#restante').textContent = restante;
+
+    }}
+   
+    const idx = new Idx();
+    //CONSULTA PARA DATOS
+    function presupuestoEstimado (){
+        const valorUsuario = prompt('INGRESE SU PRESUPUESTO MENSUAL');
+        if(valorUsuario === '' || valorUsuario === null || isNaN(valorUsuario) || valorUsuario <= 0){
+            window.location.reload();
+        }
+        presupuesto = new Presupuesto(valorUsuario);
+        idx.agregarPresupuesto(presupuesto);
+    }
+    function añadirGasto(e){
+        e.preventDefault();
+
+        const nombre = document.querySelector('#gasto').value;
+        const cantidad = Number (document.querySelector('#cantidad').value);
+//AGREGAR PARA VALIDAR LOS DATOS DEL FORMULARIO
+        const gasto = {nombre, cantidad, id: Date.now()}
+        presupuesto.newGasto(gasto);
+        idx.mostrarGasto('Gasto agregado');
+
+        const {gastos, restante} = presupuesto;
+        idx.agregarGasto(gastos);
+        idx.newRestante(restante);
+        formulario.reset();
+    }
