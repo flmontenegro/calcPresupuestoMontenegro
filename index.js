6ba36a07960945730a19fd193289dfaf7fcdde80
Florencia Montenegro <mflortrabajos@gmail.com>
2022-09-28 20:26:31 -0300

    sweetAlert

--- index.js
+++ index.js
@@ -115,7 +115,38 @@ class Idx {
     const idx = new Idx();
     //CONSULTA PARA DATOS
     function presupuestoEstimado (){
-        const valorUsuario = prompt('INGRESE SU PRESUPUESTO MENSUAL');
+        const valorUsuario = Swal.fire({
+  title: 'Submit your Github username',
+  input: 'text',
+  inputAttributes: {
+    autocapitalize: 'off'
+  },
+  showCancelButton: true,
+  confirmButtonText: 'Look up',
+  showLoaderOnConfirm: true,
+  preConfirm: (login) => {
+    return fetch(`//api.github.com/users/${login}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        return response.json()
+      })
+      .catch(error => {
+        Swal.showValidationMessage(
+          `Request failed: ${error}`
+        )
+      })
+  },
+  allowOutsideClick: () => !Swal.isLoading()
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: `${result.value.login}'s avatar`,
+      imageUrl: result.value.avatar_url
+    })
+  }
+});
         if(valorUsuario === '' || valorUsuario === null || isNaN(valorUsuario) || valorUsuario <= 0){
             window.location.reload();
         }
